5e2666ac28dd98d39eef60fe7fd1c4d801963b77
Martin Juul <martin@juul.xyz>
2018-02-18 16:06:03 +0100

    Table prototype on homepage
    Signed-off-by: Martin Juul <martin@juul.xyz>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  caption: string;
+  headings: string[];
+  footers: string[];
+  columns: Array<Array<string>>;
+
   constructor() { }
 
   ngOnInit() {
+    this.caption = 'Oversigt over hovedforløb 1';
+    this.headings = ['Fag nr.', 'Titel', 'EUD & EUV 3', 'EUV 1 & 2', 'EUX'];
+    this.footers = ['', 'Antal Uger', '10', '10', '11'];
+    this.columns = [
+      [
+        '16471',
+        'Grundlæggende programmering',
+        '2',
+        '2',
+        '2'
+      ],
+      [
+        '16476',
+        'Clientside programmering',
+        '1',
+        '1',
+        '1'
+      ],
+      [
+        '16474',
+        'Database programming',
+        '1',
+        '1',
+        '1'
+      ],
+      [
+        '16483',
+        'Versionering og dokumentation',
+        '0,5',
+        '0,5',
+        '0,5'
+      ],
+      [
+        '6223',
+        'Netværk I',
+        '1,5',
+        '1,5',
+        '1,5'
+      ],
+      [
+        '6225',
+        'Computerteknologi',
+        '1',
+        '1',
+        '1'
+      ],
+      [
+        '6226',
+        'Serverteknologi I',
+        '0',
+        '0',
+        '1'
+      ],
+      [
+        '6230',
+        'Database Server',
+        '1',
+        '1',
+        '1'
+      ]
+    ];
   }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,14 @@ import {SharedModule} from '../shared/shared.module';
 import {HomeRoutingModule} from './home-routing.module';
 
 import {HomeComponent} from './home.component';
+import {LibModule} from '../@lib/lib.module';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    LibModule
   ],
   declarations: [HomeComponent]
 })
