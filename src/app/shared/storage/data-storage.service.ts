52b68f325854d4cc4143b2e8d3ffea60c6dbd68e
Martin Juul <martin@juul.xyz>
2018-02-18 17:37:36 +0100

    Renamed data service and moved to appropriate directory.
    Signed-off-by: Martin Juul <martin@juul.xyz>

--- src/app/shared/storage/data-storage.service.ts
+++ src/app/shared/storage/data-storage.service.ts
@@ -3,17 +3,27 @@ import { HttpClient } from "@angular/common/http";
 
 import { environment } from "../../../environments/environment";
 import { ErrorService } from "../error.service";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
-export class DataFetchingService {
+export class DataStorageService {
   private api = `${environment}`;
 
   constructor(private http: HttpClient) { }
 
-  get(endPoint: string) {
+  /*get(endPoint: string) {
     return this.http.get(`${this.api}${endPoint}`,
       {
         observe: 'response'
       }).catch(ErrorService.handleError)
+  }*/
+
+  /*
+  * Only for testing before we implement the api.
+  */
+  get(endPoint: string) {
+    if (endPoint === 'semester/3') {
+      return Observable.of()
+    }
   }
 }
