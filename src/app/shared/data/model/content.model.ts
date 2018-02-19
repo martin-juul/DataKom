c98dd99de60bef72b4e0a744f06a2bd897a61ea0
Martin Juul <martin@juul.xyz>
2018-02-19 15:44:25 +0100

    Models for site content
    Signed-off-by: Martin Juul <martin@juul.xyz>

---
+++ src/app/shared/data/model/content.model.ts
@@ -0,0 +1,13 @@
+export class Header {
+  constructor(
+    name?: string,
+    image?: string
+  ) { }
+}
+
+export class Content {
+  constructor(
+    header?: Header,
+    content?: string
+  ) { }
+}
