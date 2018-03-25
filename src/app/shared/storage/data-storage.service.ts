ece848c09f6b2e549d778cb11bee9f5029edc9dd
Martin Juul <code@juul.xyz>
2018-03-25 20:21:03 +0200

    Cleanup from prototyping
    Signed-off-by: Martin Juul <code@juul.xyz>

--- src/app/shared/storage/data-storage.service.ts
+++ src/app/shared/storage/data-storage.service.ts
@@ -3,8 +3,6 @@ import { HttpClient } from "@angular/common/http";
 
 import { environment } from "../../../environments/environment";
 import { ErrorService } from "../error.service";
-import { Observable } from "rxjs/Observable";
-import 'rxjs/add/observable/of';
 
 @Injectable()
 export class DataStorageService {
@@ -12,16 +10,6 @@ export class DataStorageService {
 
   constructor(private http: HttpClient) { }
 
-  /*get(endPoint: string) {
-    return this.http.get<T>(`${this.api}${endPoint}`,
-      {
-        observe: 'response'
-      }).catch(ErrorService.handleError)
-  }*/
-
-  /*
-  * Only for testing before we implement the api.
-  */
   get(endPoint: string, params?: Object) {
 
     return this.http.get(`${this.api}${endPoint}`,
